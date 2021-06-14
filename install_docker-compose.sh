4fffc963dacd9980b97d66d13dc2e07d26bf0bb2
jmli1024 <81738188+jmli1024@users.noreply.github.com>
2021-06-14 21:13:51 +0800

    Create install_docker-compose.sh

--- install_docker-compose.sh
+++ install_docker-compose.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 install_docker_compose() {
-    curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    curl -L https://github.com/docker/compose/releases/download/1.28.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     chmod a+x /usr/local/bin/docker-compose
     docker-compose version
 }
