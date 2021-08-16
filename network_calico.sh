539609f68b251ba685f51b7ef6a775028d294ac4
jmli1024 <81738188+jmli1024@users.noreply.github.com>
2021-08-16 15:21:22 +0800

    Update network_calico.sh

--- network_calico.sh
+++ network_calico.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 #  Description: 安装网络插件calico，实现和node节点网络互通，这个只需要在master上面安装
 
+rm -f calico-3.13.1.yaml
 wget https://kuboard.cn/install-script/calico/calico-3.13.1.yaml 
 kubectl apply -f calico-3.13.1.yaml
